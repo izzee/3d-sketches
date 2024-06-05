2677ef4449d5084166aa5bd04f7f5a9231d094f7
Isaac Chabon <ichabon@me.com>
2024-06-05 12:06:30 -0400

    add error messages

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ import {
   TorusKnotGeometry,
   WebGLRenderer,
 } from 'three'
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+
+
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
@@ -22,161 +26,172 @@ import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 import { animate, inView } from 'motion'
 
 
-// Three.js
-const loaderTag = document.querySelector('section.loader')
-const threeTag = document.querySelector('section.three')
-const {width, height} = threeTag.getBoundingClientRect()
-
-let aimEffect = 0
-let currentEffect = 0
-let aimDirection = 0
-let currentDirection = 0
-let timeoutEffect
-
-const clock = new Clock()
-const scene = new Scene()
-const camera = new PerspectiveCamera( 75, width / height, 0.1, 1000 )
-
-inView('section.content', (info) => {
-  const isLeft = info.target.classList.contains('left')
-  aimDirection = isLeft ? 1 : -1
-  return (leaveInfo) => {
-    // aimDirection = 0
-  }
-}, {margin: '-25%'})
-
-const renderer = new WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setClearColor(0x000000, 0)
-threeTag.appendChild( renderer.domElement)
-
-// Post-Processing
-const composer = new EffectComposer(renderer)
-
-const renderPass = new RenderPass(scene, camera)
-composer.addPass(renderPass)
-
-const nosiePass = new ShaderPass(NoiseShader)
-nosiePass.uniforms.time.value = clock.getElapsedTime()
-nosiePass.uniforms.time.value = clock.getElapsedTime()
-nosiePass.uniforms.effect.value = currentEffect
-nosiePass.uniforms.direction.value = currentDirection
-
-nosiePass.uniforms.aspectRatio.value = width / height
-composer.addPass(nosiePass);
-
-const outputPass = new OutputPass()
-composer.addPass(outputPass)
-
-
-// Lighting
-const ambience = new AmbientLight(0x404040)
-camera.add(ambience)
-
-const keyLight = new DirectionalLight(0xffffff, 1)
-keyLight.position.set(-1,1,3)
-camera.add(keyLight)
-
-const fillLight = new DirectionalLight(0xffffff, .5)
-fillLight.position.set(1,1,3)
-camera.add(fillLight)
-
-const backLight = new DirectionalLight(0xffffff, 1)
-backLight.position.set(-1,3,-1)
-camera.add(backLight)
-
-scene.add(camera)
-
-const loadGroup = new Group()
-loadGroup.position.y = 20
-
-// Import object
-const scrollGroup = new Group()
-scrollGroup.add(loadGroup)
-
-scene.add(scrollGroup)
-
-const gltfLoader = new GLTFLoader()
-
-animate('section.new-drop', {y: -100, opacity: 0})
-
-
-gltfLoader.load('../bust.glb', (gltf) => {
-  loadGroup.add(gltf.scene)
-  // motion one animation after object loads in
-  animate((t) => {
-    loadGroup.position.y = -10 + 9 * t
-  }, { duration: 2, delay: 1})
-
-  animate('section.new-drop', 
-    {
-      y: [-100, 0],
-      opacity: [0,1]
-    }, 
-    {
-      duration: 1,
-      delay: 2,
-    }
-  )
-  animate(loaderTag, {y: '-100%'}, {duration: 1, delay:1})
-  document.body.classList.remove('loading')
-
-}, 
-  (xhr) => {
-  }, 
-  (error) => {console.error(error)
-})
-
-camera.position.z = 1.75
-
-// Controls
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableZoom = false
-controls.enablePan = false
-controls.autoRotate = true
-controls.autoRotateSpeed = 1
-
-// Animation
-const render = () => {
-  // autorotation of shape
-  controls.update()
-  // rotate scroll group containing shape
-  scrollGroup.rotation.set(0,window.scrollY*0.0025,0)
-  
-  currentDirection += (aimDirection - currentDirection) * 0.01
-  // tween effect based on scroll status
-
-  currentEffect += (aimEffect - currentEffect) * 0.05
-  nosiePass.uniforms.direction.value = currentDirection
-  nosiePass.uniforms.effect.value = currentEffect
-  nosiePass.uniforms.aspectRatio.value = width / height
-
-  // uptate time that is passed into noise shader
-  nosiePass.uniforms.time.value = clock.getElapsedTime()
-  // animate scene
-  requestAnimationFrame(render)
-	composer.render()
-}
-
-// Window resize
-const resize = () => {
+const runAnimations = () => {
+  // Three.js
+  const loaderTag = document.querySelector('section.loader')
+  const threeTag = document.querySelector('section.three')
   const {width, height} = threeTag.getBoundingClientRect()
+
+  let aimEffect = 0
+  let currentEffect = 0
+  let aimDirection = 0
+  let currentDirection = 0
+  let timeoutEffect
+
+  const clock = new Clock()
+  const scene = new Scene()
+  const camera = new PerspectiveCamera( 75, width / height, 0.1, 1000 )
+
+  inView('section.content', (info) => {
+    const isLeft = info.target.classList.contains('left')
+    aimDirection = isLeft ? 1 : -1
+    return (leaveInfo) => {
+      // aimDirection = 0
+    }
+  }, {margin: '-25%'})
+
+  const renderer = new WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setClearColor(0x000000, 0)
+  threeTag.appendChild( renderer.domElement)
+
+  // Post-Processing
+  const composer = new EffectComposer(renderer)
+
+  const renderPass = new RenderPass(scene, camera)
+  composer.addPass(renderPass)
+
+  const nosiePass = new ShaderPass(NoiseShader)
+  nosiePass.uniforms.time.value = clock.getElapsedTime()
+  nosiePass.uniforms.time.value = clock.getElapsedTime()
+  nosiePass.uniforms.effect.value = currentEffect
+  nosiePass.uniforms.direction.value = currentDirection
+
   nosiePass.uniforms.aspectRatio.value = width / height
-  camera.aspect = width / height
-  camera.updateProjectionMatrix()
-  renderer.setSize(width, height)
+  composer.addPass(nosiePass);
+
+  const outputPass = new OutputPass()
+  composer.addPass(outputPass)
+
+
+  // Lighting
+  const ambience = new AmbientLight(0x404040)
+  camera.add(ambience)
+
+  const keyLight = new DirectionalLight(0xffffff, 1)
+  keyLight.position.set(-1,1,3)
+  camera.add(keyLight)
+
+  const fillLight = new DirectionalLight(0xffffff, .5)
+  fillLight.position.set(1,1,3)
+  camera.add(fillLight)
+
+  const backLight = new DirectionalLight(0xffffff, 1)
+  backLight.position.set(-1,3,-1)
+  camera.add(backLight)
+
+  scene.add(camera)
+
+  const loadGroup = new Group()
+  loadGroup.position.y = 20
+
+  // Import object
+  const scrollGroup = new Group()
+  scrollGroup.add(loadGroup)
+
+  scene.add(scrollGroup)
+
+  const gltfLoader = new GLTFLoader()
+
+  animate('section.new-drop', {y: -100, opacity: 0})
+
+
+  gltfLoader.load('../bust.glb', (gltf) => {
+    loadGroup.add(gltf.scene)
+    // motion one animation after object loads in
+    animate((t) => {
+      loadGroup.position.y = -10 + 9 * t
+    }, { duration: 2, delay: 1})
+
+    animate('section.new-drop', 
+      {
+        y: [-100, 0],
+        opacity: [0,1]
+      }, 
+      {
+        duration: 1,
+        delay: 2,
+      }
+    )
+    animate(loaderTag, {y: '-100%'}, {duration: 1, delay:1})
+    document.body.classList.remove('loading')
+
+  }, 
+    (xhr) => {
+    }, 
+    (error) => {console.error(error)
+  })
+
+  camera.position.z = 1.75
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableZoom = false
+  controls.enablePan = false
+  controls.autoRotate = true
+  controls.autoRotateSpeed = 1
+
+  // Animation
+  const render = () => {
+    // autorotation of shape
+    controls.update()
+    // rotate scroll group containing shape
+    scrollGroup.rotation.set(0,window.scrollY*0.0025,0)
+    
+    currentDirection += (aimDirection - currentDirection) * 0.01
+    // tween effect based on scroll status
+
+    currentEffect += (aimEffect - currentEffect) * 0.05
+    nosiePass.uniforms.direction.value = currentDirection
+    nosiePass.uniforms.effect.value = currentEffect
+    nosiePass.uniforms.aspectRatio.value = width / height
+
+    // uptate time that is passed into noise shader
+    nosiePass.uniforms.time.value = clock.getElapsedTime()
+    // animate scene
+    requestAnimationFrame(render)
+    composer.render()
+  }
+
+  // Window resize
+  const resize = () => {
+    const {width, height} = threeTag.getBoundingClientRect()
+    nosiePass.uniforms.aspectRatio.value = width / height
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+    renderer.setSize(width, height)
+  }
+  window.addEventListener('resize', resize)
+
+  // Scroll listener
+  const scroll = () => {
+    clearTimeout(timeoutEffect)
+    aimEffect = 1
+
+    timeoutEffect = setTimeout(() => {
+      aimEffect = 0
+    }, 100)
+  }
+  window.addEventListener('scroll', scroll)
+
+  render()
 }
-window.addEventListener('resize', resize)
 
-// Scroll listener
-const scroll = () => {
-  clearTimeout(timeoutEffect)
-  aimEffect = 1
+if ( WebGL.isWebGLAvailable() ) {
+  runAnimations()
+	// Initiate function or other initializations here
 
-  timeoutEffect = setTimeout(() => {
-    aimEffect = 0
-  }, 100)
+} else {
+  document.querySelector('.error-message').style.display = 'block'
+
 }
-window.addEventListener('scroll', scroll)
-
-render()
--- spiral.js
+++ spiral.js
@@ -12,109 +12,123 @@ import {
   WebGLRenderer,
 } from 'three'
 
-const renderer = new WebGLRenderer({
-  antialias: true
-})
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setClearColor(0xffff00, 1) // first argument is color, second is opacity
-document.querySelector('section').appendChild(renderer.domElement)
 
-const scene = new Scene()
+const runAnimations = () => {
+  const renderer = new WebGLRenderer({
+    antialias: true
+  })
 
-const camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 5000)
-camera.position.z = -50
-camera.lookAt(scene.position)
-
-const light = new DirectionalLight(0xffffff, 1)
-light.position.set(0,0,-1)
-
-scene.add(light)
-
-const shapes = []
-
-const animate = function() {
-  renderer.render(scene, camera)
-  requestAnimationFrame(animate)
-  shapes.forEach((shape, index) => {
-    shape.rotateX(Math.random()/100)
-    shape.position.setZ(shape.position.z + 5)
-    if (shape.position.z >= 6000) { 
-      shape.position.z = 7000
-      scene.remove(shape)
-      shapes[index].remove()
-      return
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setClearColor(0xffff00, 1) // first argument is color, second is opacity
+  document.querySelector('section').appendChild(renderer.domElement)
+  
+  const scene = new Scene()
+  
+  const camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 5000)
+  camera.position.z = -50
+  camera.lookAt(scene.position)
+  
+  const light = new DirectionalLight(0xffffff, 1)
+  light.position.set(0,0,-1)
+  
+  scene.add(light)
+  
+  const shapes = []
+  
+  const animate = function() {
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+    shapes.forEach((shape, index) => {
+      shape.rotateX(Math.random()/100)
+      shape.position.setZ(shape.position.z + 5)
+      if (shape.position.z >= 6000) { 
+        shape.position.z = 7000
+        scene.remove(shape)
+        shapes[index].remove()
+        return
+      }
+    })
+  }
+  animate()
+  
+  let growth = 0
+  let direction = 1
+  
+  const createShape = function(x,y) {
+    growth += direction * 10
+    if (growth >= 100) {
+      direction = -1
+    } else {
+      direction = +1
+    }
+    const geometries = [
+      new BoxGeometry(150, 150, 150),
+      // new ConeGeometry(100, 150, 320),
+      // new SphereGeometry(50 + growth, 50 + growth, 50 + growth)
+    ]
+  
+    const randomNumber = Math.floor(Math.random() * geometries.length)
+    const color = new Color(`hsl(${Math.random()*255}, ${Math.random()*50}%, ${Math.random()*25}%)`)
+    const emmisiveColor = new Color(`hsl(${Math.random()*255}, ${Math.random()*100}%, ${Math.random()*100}%)`)
+  
+    const material = new MeshLambertMaterial({
+      color: color,
+      emissive: emmisiveColor
+    })
+    const shape = new Mesh(geometries[randomNumber], material)
+    shape.rotateX(Math.random() * 10)
+    shape.rotateZ(Math.random() * 10)
+  
+    shape.position.set((window.innerWidth/2) - x, (window.innerHeight/2) - y, camera.position.z + 500)
+  
+    shapes.push(shape)
+    scene.add(shape)
+  }
+  
+  let mouseIsDown = false
+  
+  
+  document.addEventListener('mousemove', event => {
+    createShape(event.pageX,event.pageY)
+  })
+  document.addEventListener('mousedown', () => {
+    mouseIsDown = true
+  })
+  
+  document.addEventListener('mouseup', () => {
+    mouseIsDown = false
+  })
+  
+  document.addEventListener('touchmove', event => {
+    if (mouseIsDown) {
+      createShape(event.pageX,event.pageY)
     }
   })
-}
-animate()
-
-let growth = 0
-let direction = 1
-
-const createShape = function(x,y) {
-  growth += direction * 10
-  if (growth >= 100) {
-    direction = -1
-  } else {
-    direction = +1
-  }
-  const geometries = [
-    // new ConeGeometry(100, 150, 320),
-    new BoxGeometry(150, 150, 150),
-    // new SphereGeometry(50 + growth, 50 + growth, 50 + growth)
-  ]
-
-  const randomNumber = Math.floor(Math.random() * geometries.length)
-  const color = new Color(`hsl(${Math.random()*255}, ${Math.random()*50}%, ${Math.random()*25}%)`)
-  const emmisiveColor = new Color(`hsl(${Math.random()*255}, ${Math.random()*100}%, ${Math.random()*100}%)`)
-
-  const material = new MeshLambertMaterial({
-    color: color,
-    emissive: emmisiveColor
+  document.addEventListener('touchstart', () => {
+    mouseIsDown = true
   })
-  const shape = new Mesh(geometries[randomNumber], material)
-  shape.rotateX(Math.random() * 10)
-  shape.rotateZ(Math.random() * 10)
-
-  shape.position.set((window.innerWidth/2) - x, (window.innerHeight/2) - y, camera.position.z + 500)
-
-  shapes.push(shape)
-  scene.add(shape)
+  
+  document.addEventListener('touchend', () => {
+    mouseIsDown = false
+  })
+  
+  window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / this.window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  })
 }
 
-let mouseIsDown = false
 
 
-document.addEventListener('mousemove', event => {
-  // if (mouseIsDown) {
-    createShape(event.pageX,event.pageY)
-  // }
-})
-document.addEventListener('mousedown', () => {
-  mouseIsDown = true
-})
+if ( WebGL.isWebGLAvailable() ) {
+  runAnimations()
+	// Initiate function or other initializations here
 
-document.addEventListener('mouseup', () => {
-  mouseIsDown = false
-})
+} else {
+  document.querySelector('.error-message').style.display = 'block'
 
-document.addEventListener('touchmove', event => {
-  if (mouseIsDown) {
-    createShape(event.pageX,event.pageY)
-  }
-})
-document.addEventListener('touchstart', () => {
-  mouseIsDown = true
-})
-
-document.addEventListener('touchend', () => {
-  mouseIsDown = false
-})
-
-window.addEventListener('resize', function () {
-  camera.aspect = window.innerWidth / this.window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-})
+}
